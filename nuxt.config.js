08014db8792c199ffe7a5df82429a707352d2429
Ngac Duy Anh <ngac.duyanh@gmail.com>
2021-05-16 22:49:43 +0700

    init setup

---
+++ nuxt.config.js
@@ -0,0 +1,34 @@
+export default {
+  target: 'static',
+
+  head: {
+    title: 'blog',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  css: [],
+
+  plugins: [],
+
+  components: true,
+
+  buildModules: [
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/stylelint-module',
+    '@nuxtjs/tailwindcss',
+  ],
+
+  modules: ['@nuxt/content'],
+
+  content: {},
+
+  build: {},
+}
